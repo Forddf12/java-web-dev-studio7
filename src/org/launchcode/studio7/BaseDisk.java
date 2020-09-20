d6a87fa54098bd79ad16cda0fa472b6f955ae309
Doug F <forddf12@outlook.com>
2020-09-20 15:21:11 -0500

    Need to turn in

---
+++ src/org/launchcode/studio7/BaseDisk.java
@@ -0,0 +1,12 @@
+package org.launchcode.studio7;
+
+public abstract class BaseDisk {
+    int maxStorage(){
+
+    };
+
+    int diskSpeed() {
+
+    };
+
+}
---
+++ src/org/launchcode/studio7/OpticalDisk.java
@@ -0,0 +1,11 @@
+package org.launchcode.studio7;
+
+public interface OpticalDisk {
+    void spinDisk();
+    void storageCapacity();
+    void writeData();
+    void readData();
+    void loadedInfo();
+    void eraseData();
+    void immunityMagnet();
+}
